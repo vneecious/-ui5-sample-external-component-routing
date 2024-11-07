ed2b2d8cf9fee362de693a7440d2a0b08114c6c4
vinicius <vinicius@makeamatch.com.br>
2024-11-07 12:03:25 -0300

    feat: adding navigation to external ui5 component

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,14 +1,12 @@
-sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function(BaseController) {
-      "use strict";
-  
-      return BaseController.extend("my.routingexternalcomponent.controller.App", {
-        onInit: function() {
-        }
-      });
-    }
-  );
-  
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (BaseController) {
+  "use strict";
+
+  return BaseController.extend("my.routingexternalcomponent.controller.App", {
+    onInit: function () {},
+    onItemSelect: function (oEvent) {
+      const oItem = oEvent.getParameter("item");
+      var oRouter = this.getOwnerComponent().getRouter();
+      oRouter.navTo(oItem.getKey());
+    },
+  });
+});
--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,12 +1,19 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-function (Controller) {
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/IllustrationPool"],
+  function (Controller, IllustrationPool) {
     "use strict";
 
     return Controller.extend("my.routingexternalcomponent.controller.Main", {
-        onInit: function () {
-
-        }
+      onInit: function () {
+        // registra las ilustraciones de TNT (tnt-)
+        IllustrationPool.registerIllustrationSet(
+          {
+            setFamily: "tnt",
+            setURI: sap.ui.require.toUrl("sap/tnt/themes/base/illustrations"),
+          },
+          false
+        );
+      },
     });
-});
+  }
+);

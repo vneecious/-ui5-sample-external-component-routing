07b50a65229462563b2e4a683029aacba1a0cfb9
vinicius <vinicius@makeamatch.com.br>
2024-11-07 11:40:24 -0300

    init

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("my.routingexternalcomponent.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"my/routing-external-component/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
